1d12bb92cf23a3cf03725db0fc45304fb0ff24ef
Serhat Merak <merakserhat@gmail.com>
2024-03-09 21:19:46 +0300

    feat: initialize

---
+++ src/test/java/com/serhatmerak/chain/ChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhatmerak.chain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
